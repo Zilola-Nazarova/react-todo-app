3900b8824cc53589712bb651470583e1e1c9400c
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-15 11:23:00 +0500

    Set up project from scratch (usinb webpack, babel, react)

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reactLogo from './images/react.svg';
+import './styles/app.css';
+
+const rootElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <div>
+    <h1>Hello from React application</h1>
+    <img src={reactLogo} className="logo react" alt="React logo" />
+  </div>
+);
